f1499de390271065338ef92a737b0b211ecbe8b5
Andre Karner <info@andre-k.at>
2017-11-03 22:25:12 +0100

    Add project files.

---
+++ Dojo1/Custom/DojoStack.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dojo1.Custom
+{
+    class DojoStackNode<T>
+    {
+        public T Value { get; }
+        public DojoStackNode<T> Prev { get; }
+
+        public DojoStackNode(T val, DojoStackNode<T> prev)
+        {
+            this.Value = val;
+            this.Prev = prev;
+        }
+    }
+
+
+    public class DojoStack<T>
+    {
+        
+        private DojoStackNode<T> _current = null;
+
+
+        public DojoStack()
+        {
+            
+        }
+
+        public DojoStack(T obj)
+        {
+            this.Push(obj);
+        }
+
+        public void Push(T obj)
+        {
+            if (this._current == null)
+            {
+                this._current = new DojoStackNode<T>(obj, null);
+            }
+            else
+            {
+                var newNode = new DojoStackNode<T>(obj, this._current);
+                this._current = newNode;
+            }
+            
+        }
+
+        public T Pop()
+        {
+            if (this._current == null)
+                throw new Exception("Stack has no elements!");
+
+            var val = this._current.Value;
+
+            this._current = this._current.Prev;
+
+            return val;
+        }
+
+        public T Peek()
+        {
+            if (this._current == null)
+                throw new Exception("Stack has no elements!");
+
+            return this._current.Value;
+        }
+
+
+    }
+}
---
+++ Dojo1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dojo1.Custom;
+
+namespace Dojo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var intStack = new DojoStack<int>(10);
+
+            try
+            {
+                intStack.Push(20);
+                intStack.Push(30);
+                intStack.Push(40);
+                intStack.Push(50);
+                intStack.Push(60);
+
+            
+                Console.WriteLine(intStack.Peek());
+                intStack.Push(30);
+
+                Console.WriteLine(intStack.Peek());
+
+                Console.WriteLine(intStack.Pop());
+
+                Console.WriteLine(intStack.Pop());
+
+                Console.WriteLine(intStack.Pop());
+
+                Console.WriteLine(intStack.Pop());
+
+                Console.WriteLine(intStack.Pop());
+
+                Console.WriteLine(intStack.Pop());
+
+                Console.WriteLine(intStack.Pop());
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occured: " + ex.Message);
+            }
+
+
+            Console.Read();
+
+
+        }
+    }
+}
